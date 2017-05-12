56df7b6b1fc5bdb60a0fc28d1ec4979e21f7fb94
Brian Lee <brian112358@gmail.com>
2017-05-11 22:24:10 -0700

    Finished up scraper.py and added top 100 scraped posts from /r/wholesomememes. Each line in the scraped text contains a single comment's parent and the comment itself. Parent and comment bodies are separated by \x0b (vertical tab). Newlines in comments are replaced with \r to comply with the one-comment-per-line format.

--- project/scraper.py
+++ project/scraper.py
@@ -1,6 +1,20 @@
 import praw
+from praw.models import MoreComments, Submission
+import os
+
+num_posts = 100
+score_threshold = 10
+parent_child_demarcator = '\x0b' # vertical tab
+scraped_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'data/scraped')
 
 # un: 87project; pw: in our hangouts chat
+
+def sanitize(text):
+    sanitized = text.encode('ascii', 'ignore').decode('utf-8')
+    # Since comments are stored one per line, we need to replace newlines with
+    # another character: tab character.
+    sanitized = sanitized.replace('\n', '\t')
+    return sanitized
 
 # obtaining a reddit instance (required)
 # do not change these values
@@ -11,8 +25,8 @@ reddit = praw.Reddit(client_id='FFkqA8RP6rqvbw',
 # assume you have a Reddit instance bound to variable `reddit`
 subreddit = reddit.subreddit('wholesome' 'memes')
 
-print(subreddit.display_name)  # Output: wholesomememes
-print(subreddit.title)         # Output: Internet for the Spirit
+print("Scraping:", subreddit.display_name)  # Output: wholesomememes
+print(subreddit.title)                      # Output: Internet for the Spirit
 print()
 
 # be ready to decode to ascii to handle unicode (since reddit is OK with
@@ -20,16 +34,30 @@ print()
 # use .encode('ascii', 'ignore').decode('utf-8')
 
 # iterate through the top 100 posts of all time
-num_posts = 1
+post_num = 1
 for submission in subreddit.top(limit=num_posts):
-	s_title = submission.title.encode('ascii', 'ignore').decode('utf-8')
-	# default comment sort is 'best'
-	top_level_comments = list(submission.comments)
-	print(s_title)
-	print(top_level_comments)
-
-
-
-
-
-
+    s_title = sanitize(submission.title)
+    # default comment sort is 'best'
+    print("Scraping submission (%d/%d):" % (post_num, num_posts), s_title)
+    submission.comments.replace_more()
+    num_comments = 0
+    with open(os.path.join(scraped_path, submission.id), 'w') as f:
+        for comment in submission.comments.list():
+            if isinstance(comment, MoreComments) \
+                    or comment.score < score_threshold:
+                continue
+            line = ''
+            parent = comment.parent()
+            if isinstance(parent, Submission):
+                line += s_title
+            else:
+                if parent.body == '[deleted]':
+                    continue
+                line += sanitize(parent.body)
+            line += parent_child_demarcator
+            line += sanitize(comment.body)
+            line += '\n'
+            f.write(line)
+            num_comments += 1
+    print("Done scraping %d comments" % num_comments)
+    post_num += 1
