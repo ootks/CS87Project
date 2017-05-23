7910427cc0be132fd0797fc5b23735ef4e063b5d
Kevin Shu <shu.kevins@gmail.com>
2017-05-22 21:30:59 -0700

    Added homework5

---
+++ hw5/replacements.py
@@ -0,0 +1,18 @@
+from nltk.corpus import wordnet as wn
+from nltk.corpus import sentiwordnet as swn
+
+#Produces an synonym for the given word
+def synonym(word):
+    pass
+
+#Produces an antonym for the given word
+def antonym(word):
+    pass
+
+#Produces an hyponym for the given word
+def hyponym(word):
+    pass
+
+#Produces an hypernym for the given word
+def hyponym(word):
+    pass
